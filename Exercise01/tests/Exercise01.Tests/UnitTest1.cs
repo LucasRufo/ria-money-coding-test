b7cf84c7a32ef9f9842ee53b51a43b37ad147b44
Lucas Rufo <rifelu25@outlook.com>
2024-02-14 16:32:45 -0300

    feat: creates exercise 01 project

---
+++ Exercise01/tests/Exercise01.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Exercise01.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
