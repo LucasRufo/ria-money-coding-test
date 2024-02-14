0ae47d3dbb315b7795c548eb1f311908ae95c3d4
Lucas Rufo <rifelu25@outlook.com>
2024-02-14 20:28:26 -0300

    feat: create customer entity

---
+++ Exercise02/src/Exercise02.Domain/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise02.Domain.Entities;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public int Age { get; set; }
+}
